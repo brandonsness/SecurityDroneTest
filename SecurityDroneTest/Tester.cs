7c7e9fb0bd961d62bb9b1375399ee56c4c6458aa
Noah Boeckman <noahboeckman@gmail.com>
2021-11-11 10:47:37 -0600

    Add class for generating RSA public/private keys.

--- SecurityDroneTest/Tester.cs
+++ SecurityDroneTest/Tester.cs
@@ -6,9 +6,13 @@ namespace SecurityDroneTest
     {
         public static void Main(string [] args)
         {
+            // Demonstrating public/private key generation. Can delete later.
+            DroneRSA keys = new DroneRSA();
+            Console.WriteLine(BitConverter.ToString(keys.rsaKeyInfo.Exponent));
+            Console.WriteLine(BitConverter.ToString(keys.rsaKeyInfo.D));
+
             Drone drone = new Drone();
             DroneController control = new DroneController();
-
         }
     }
 }
