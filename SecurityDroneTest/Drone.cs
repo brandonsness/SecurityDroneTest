bc8b02bf73324673444c9a8c92f906e80df3a485
brandonsness <brandonsness@users.noreply.github.com>
2021-11-12 09:48:06 -0600

    Get public ip now

--- SecurityDroneTest/Drone.cs
+++ SecurityDroneTest/Drone.cs
@@ -23,8 +23,8 @@ namespace SecurityDroneTest
             TcpListener drone = null;
 
             //Get first ipv4 addr 
-            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = host.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            string externalIpString = new System.Net.WebClient().DownloadString("https://api.ipify.org");
+            IPAddress ipAddress = IPAddress.Parse(externalIpString);
 
             try
             {
